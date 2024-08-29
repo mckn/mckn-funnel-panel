45b95a63a681fc291680e8bd3b5bae4364042bc1
Marcus Andersson <marcus.andersson@grafana.com>
2024-08-29 11:05:30 +0200

    fixed linting warnings.

--- src/components/Unsupported.tsx
+++ src/components/Unsupported.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/css';
-import { Alert, HorizontalGroup, useStyles2 } from '@grafana/ui';
+import { Alert, Stack, useStyles2 } from '@grafana/ui';
 import React, { type ReactElement } from 'react';
 
 export function Unsupported(): ReactElement {
@@ -7,12 +7,12 @@ export function Unsupported(): ReactElement {
 
   return (
     <div className={styles.container}>
-      <HorizontalGroup justify="center" align="center">
+      <Stack justifyContent="center" alignItems="center">
         <Alert severity="info" title="Unsupported data">
           The data you have provided is not supported by this panel. Every data frame provided to the panel needs to
           contain at least one numeric field which will be used to visualize each step in the funnel.
         </Alert>
-      </HorizontalGroup>
+      </Stack>
     </div>
   );
 }
